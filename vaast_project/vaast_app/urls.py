9193b52ad7a337da8b616b952d20d98ff3a7ee21
noswaldSCSU <143564404+noswaldSCSU@users.noreply.github.com>
2024-06-01 20:23:38 -0400

    url update

--- vaast_project/vaast_app/urls.py
+++ vaast_project/vaast_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register_participant, name='register_participant'),
